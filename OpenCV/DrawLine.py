b2755d67ecafd9b1d454c3f4e249ceb7efb1a132
Hank <b00504059@ntu.edu.tw>
2018-03-29 19:52:56 +0900

    Drawline

---
+++ OpenCV/DrawLine.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+img = np.zeros((512,512,3), np.uint8)
+for i in range(5):
+    cv2.line(img,(10+10*i,10),(20+10*i,10),(255,0,0),2)
+    cv2.line(img,(20+10*i,10),(20+10*i,20),(255,0,0),2)
+    cv2.line(img,(20+10*i,20),(10+10*i,20),(255,0,0),2)
+cv2.imshow('BY', img)
+cv2.waitKey(0)
