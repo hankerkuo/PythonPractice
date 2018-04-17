51c38b496c85bbb65a7f9f9398fe83c726dd280c
Hank <b00504059@ntu.edu.tw>
2018-04-17 21:51:04 +0900

    Mo-Fan

--- temporary.py
+++ temporary.py
@@ -1,12 +1,7 @@
 import numpy as np
 
-scores = np.array([1, 2, 3])
+a = np.array([[1, 2, 3], [4, 5, 6]])
+b = np.array([[7, 8, 9], [10, 11, 12]])
 
-def test(*x):
-    return x
-
-
-sequence = test(scores)
-print(sequence)
-for i in sequence:
-    print(i)
+for i, k in zip(a, b):
+    print(i + k)
--- tensorflow/MoFanTutorial/Mo_Fan_CNN.py
+++ tensorflow/MoFanTutorial/Mo_Fan_CNN.py
@@ -51,16 +51,16 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
 h_pool1 = max_pool_2x2(h_conv1)                                         # output size 14x14x32
 
 ## conv2 layer ##
-W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5,5, 32, 128]) # patch 5x5, in size 32, out size 64
+b_conv2 = bias_variable([128])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
 h_pool2 = max_pool_2x2(h_conv2)                                         # output size 7x7x64
 
 ## fc1 layer ##
-W_fc1 = weight_variable([7*7*64, 1024])
+W_fc1 = weight_variable([7*7*128, 1024])
 b_fc1 = bias_variable([1024])
 # [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*128])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
@@ -90,5 +90,8 @@ for i in range(10000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
     if i % 50 == 0:
-        print(compute_accuracy(
+        print(i, 'th', compute_accuracy(
             mnist.test.images[:1000], mnist.test.labels[:1000]))
+        if compute_accuracy(mnist.test.images[:1000], mnist.test.labels[:1000]) >= 0.98:
+            print('reached accuracy', compute_accuracy(mnist.test.images[:1000], mnist.test.labels[:1000]))
+            break
