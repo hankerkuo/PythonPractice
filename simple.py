31df9cf74fa8ef74fa674d7aa92409a1db7b310a
Hank <b00504059@ntu.edu.tw>
2018-05-18 21:50:32 +0900

    1

--- simple.py
+++ simple.py
@@ -1,29 +1,12 @@
 import numpy as np
 
-input = np.array([[0, 0],
-                  [0, 1],
-                  [1, 0],
-                  [1, 1]])
+class AlexNet(object):
 
+    def __init__(self, x):
+        self.X = x
+        self.create()
 
-def layer1(x1, x2):
-    output1 = -0.5 + x1 + x2
-    output2 = 1.5 - x1 - x2
-    if output1 >= 0:
-        output1 = 1
-    else:
-        output1 = -1
-    if output2 >= 0:
-        output2 = 1
-    else:
-        output2 = -1
-    return output1, output2
+    def create(self):
+        self.fc8 = 123
 
-
-def output_layer(x1, x2):
-    return x1 + x2 - 1
-
-
-for i in range(4):
-    # print(layer1(input[i][0], input[i][1]))
-    print(output_layer(*layer1(input[i][0], input[i][1])))
+print(AlexNet(10).fc8)
