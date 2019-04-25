539797ab292e8531a963f9cac344929f1f9d0c25
Hank-NB <b00504059@ntu.edu.tw>
2019-04-26 00:40:59 +0900

    cpp

---
+++ ForHomeWork/0425.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+#include "string.h"
+
+void permutation(char str[100], char show[100], unsigned int str_length){
+ if (strlen(show) == str_length){
+     for(int i = 0; i < str_length; i++){
+       printf("%c", show[i]);
+      }
+     printf("\n");
+ }
+ else{
+ for (unsigned int i = 0; i < strlen(str); i++){
+     char str_temp[100] = "";
+     char show_temp[100] = "";
+
+     strcpy(show_temp, show);
+     show_temp[strlen(show)] = str[i];
+
+     int count = 0;
+     for(unsigned int j = 0; j < strlen(str); j++){
+         if(j == i) continue;
+         str_temp[count] = str[j];
+         count ++;
+      }
+     permutation(str_temp, show_temp, str_length);
+     }
+ }
+}
+
+int main()
+{
+  char str[100] ="xyz";
+  char show[100] = "";
+  unsigned int str_length = strlen(str);
+  permutation(str, show, str_length);
+  return 0;
+}
