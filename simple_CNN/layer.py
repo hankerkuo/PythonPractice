5d14836210ddc210bc9ad6b40cfb04f123910f13
Hank <b00504059@ntu.edu.tw>
2019-12-02 20:37:16 +0900

    CNN layer first attempt, still now only support the protocol between CNN and FC, still need to do with CNN and CNN

--- simple_CNN/layer.py
+++ simple_CNN/layer.py
@@ -6,20 +6,19 @@ import platform
 class FC:
 
     # nodes is the hidden nodes number
-    def __init__(self, batch, feature_len, nodes, activation):
+    def __init__(self, in_nodes, nodes, activation):
         self.activation = activation
-        self.w = np.random.rand(nodes, feature_len) - 0.5
+        self.w = np.random.rand(nodes, in_nodes) - 0.5
         self.b = np.zeros(nodes, dtype=float)
-        self.z = np.empty((batch, nodes), dtype=float)
-        self.a = np.empty((batch, nodes), dtype=float)
     
     def forward_prop(self, input):
         self.z = np.matmul(input, self.w.T) + self.b
         self.a = getattr(Activations(), self.activation)(self.z)
+        self.a_previous = input
 
         return self.a
 
-    def back_prop(self, label=False, w_nextlayer=None, delta_nextlayer=None, a_previouslayer=None):
+    def back_prop(self, label=False, w_nextlayer=None, delta_nextlayer=None):
 
         deri_a_wrt_z = getattr(Activations(), self.activation + '_derivative')(self.z)
 
@@ -27,15 +26,80 @@ class FC:
             # delta's first axis is also batch number, be familiar with matmul expression
             delta = np.squeeze(np.matmul(w_nextlayer.T, np.expand_dims(delta_nextlayer, axis=2)), axis=-1) * deri_a_wrt_z
         else:
-            delta = Loss().CE_derivative(label, self.a) * deri_a_wrt_z
+            delta = Loss().MSE_derivative(label, self.a) * deri_a_wrt_z
         
-        # gradient_w = np.average(np.matmul(np.expand_dims(a_previouslayer, axis=2), np.expand_dims(delta, axis=1)), axis=0).T
-        gradient_w = np.average(np.matmul(np.expand_dims(delta, axis=2), np.expand_dims(a_previouslayer, axis=1)), axis=0)
+        gradient_w = np.average(np.matmul(np.expand_dims(delta, axis=2), np.expand_dims(self.a_previous, axis=1)), axis=0)
         gradient_b = np.average(delta, axis=0)
         self.w -= 0.1 * gradient_w
         self.b -= 0.1 * gradient_b
 
         return self.w, delta
+
+
+class Conv1D:
+
+    # 1D conv, filter size is a integer
+    def __init__(self, filter_size, channels, padding, stride, activation):
+        self.filter_size = filter_size
+        self.channels = channels
+        self.padding = padding
+        self.stride = stride
+        self.activation = activation
+        self.w = np.random.rand(channels, filter_size) - 0.5
+        self.b = np.zeros(channels, dtype=float)
+    
+    def forward_prop(self, input):
+        self.batch = np.shape(input)[0]
+        self.output_dim = np.ceil(np.shape(input)[1] / self.stride).astype('int')
+        self.z = np.empty((self.channels, self.batch, self.output_dim))
+
+        if self.padding == 'same':
+            input = self.same_padding(input)
+
+        self.a_previous = input
+
+        # each channel
+        for i, w in enumerate(self.w):
+            # each output node
+            for k in range(self.output_dim):
+                self.z[i, :, k] = np.dot(input[:, k * self.stride: k * self.stride + self.filter_size], w) + self.b[i]
+        self.a = getattr(Activations(), self.activation)(self.z)
+
+        # flatten the output
+        self.a = np.swapaxes(self.a, 0, 1)
+        self.a = np.reshape(self.a, (self.batch, -1))
+
+        return self.a
+    
+    def back_prop(self, label=False, w_nextlayer=None, delta_nextlayer=None):
+        
+        delta = np.empty((self.channels, self.batch, self.output_dim))
+        gradient_w = np.zeros((self.channels, self.batch, self.filter_size))
+
+        # each channel
+        for i, z in enumerate(self.z):
+            deri_a_wrt_z = getattr(Activations(), self.activation + '_derivative')(z)
+            w_next = w_nextlayer[:, i * self.output_dim: (i + 1) * self.output_dim]
+            delta[i, :, :] = np.squeeze(np.matmul(w_next.T, np.expand_dims(delta_nextlayer, axis=2)), axis=-1) * deri_a_wrt_z
+            # each output node
+            for k in range(self.output_dim):
+                gradient_w[i] += np.multiply(np.expand_dims(delta[i, :, k], axis=1), 
+                                            self.a_previous[:, k * self.stride: k * self.stride + self.filter_size])
+            self.w[i] -= 0.1 * np.average(gradient_w[i], axis=0)
+            self.b[i] -= 0.1 * np.average(delta[i])
+
+        return self.w, delta
+
+    def same_padding(self, input):
+        if self.filter_size % 2 != 0:
+            pad_left = (self.filter_size - 1) // 2
+            pad_right = (self.filter_size - 1) // 2
+            return np.pad(input, ((0, 0), (pad_left, pad_right)), mode='constant')
+        else:
+            pad_left = self.filter_size // 2
+            pad_right = self.filter_size // 2 + 1
+            return np.pad(input, ((0, 0), (pad_left, pad_right)), mode='constant')             
+            
 
 
 class Activations:
@@ -79,26 +143,26 @@ if __name__ == '__main__':
         test_folder, 162, (16, 16), class_num=class_num)
     valid_x, valid_y = valid_data_gen.load_data()
 
-    layer_1 = FC(batch, 256, 12, 'sigmoid')
-    layer_2 = FC(batch, 12, 10, 'sigmoid')
+    conv_1 = Conv1D(3, 5, 'same', 2, 'sigmoid')
+    fc_1 = FC(128 * 5, 10, 'sigmoid')
 
     for i in range(1000000):
 
         x, y = data_generator.load_data()
         x = np.reshape(x, (batch, 16 * 16))
 
-        layer1_a = layer_1.forward_prop(x)
-        layer2_a = layer_2.forward_prop(layer1_a)
+        x = conv_1.forward_prop(x)
+        x = fc_1.forward_prop(x)
 
-        layer2_w, layer2_delta = layer_2.back_prop(label=y, a_previouslayer=layer1_a)
-        layer1_w, layer1_delta = layer_1.back_prop(w_nextlayer=layer2_w, delta_nextlayer=layer2_delta, a_previouslayer=x)
+        w, delta = fc_1.back_prop(label=y)
+        conv_1.back_prop(w_nextlayer=w, delta_nextlayer=delta)
 
-        if (i + 1) % 100 == 0:
+        if (i + 1) % 10 == 0:
             true_prediction = 0
             for (x, y) in zip(valid_x, valid_y):
                 x = np.reshape(x, (1, 16 * 16))
-                layer1_out = layer_1.forward_prop(x)
-                layer2_out = layer_2.forward_prop(layer1_out)
-                if np.argmax(y) == np.argmax(layer2_out):
+                conv1_out = conv_1.forward_prop(x)
+                fc1_out = fc_1.forward_prop(conv1_out)
+                if np.argmax(y) == np.argmax(fc1_out):
                     true_prediction += 1
             print('ite:{}, acc={}'.format((i + 1), true_prediction / 162))
