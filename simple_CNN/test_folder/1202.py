cc5a974e5a9469ce2b038c5da90be84d0b35b9dc
Hank-NB <b00504059@ntu.edu.tw>
2019-12-17 12:14:08 +0900

    test files

--- simple_CNN/test_folder/1202.py
+++ simple_CNN/test_folder/1202.py
@@ -8,8 +8,13 @@ w = np.array([
 ])
 
 a = np.ones((16, 5, 5))
+b = np.arange(16 * 5 * 5)
+b = np.reshape(b, (16, 5, 5))
+c = a * b
+
 
 # output_dim = 5
 # w_flattern = np.repeat(w, output_dim, axis=0)
 
-print(np.sum(a, axis=(0, 1)))
+print(np.shape(c))
+# print(np.rot90(b, k=2, axes=(1, 2)))
