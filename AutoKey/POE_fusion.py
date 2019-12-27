46e0a52701ef99e914118d943e89639c63f09e33
Hank <b00504059@ntu.edu.tw>
2019-12-27 14:13:45 +0900

    water and fusion update

--- AutoKey/POE_fusion.py
+++ AutoKey/POE_fusion.py
@@ -5,11 +5,12 @@ import time
 # press right shift to turn on and turn off
 
 # key_code for continuously pressing, 42 for shift, 29 for control
-key_code = 42
+key_code = 29
 print('POE fusion for RKN running ... on/off: scroll lock')
 while 1:
-    # scroll lock for starting the process
-    keyboard.wait(70)
+    # scroll lock(70) or F4(62) for starting the process
+    on_off_key = 62
+    keyboard.wait(on_off_key)
     frequency = 2000  # Set Frequency To 2000 Hertz
     duration = 100  # Set Duration, 1000 ms == 1 second
     for i in range(3):
@@ -17,7 +18,7 @@ while 1:
 
     keyboard.press(key_code)
     while 1:
-        if keyboard.is_pressed(70):
+        if keyboard.is_pressed(on_off_key):
             break
         time.sleep(0.05)
         mouse.click()
